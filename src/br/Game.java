e22cd4a12e9eee3fbd0bed9cb580f7231ca849cb
Everson Silva <eversonmessias@outlook.com>
2022-08-05 00:44:46 -0300

    Voltando a Ativa

---
+++ src/br/Game.java
@@ -0,0 +1,12 @@
+package br;
+
+import br.display.Display;
+
+public class Game {
+	
+	private Display display;
+	
+	public Game() {
+		display = new Display("Pong", 400, 300);
+	}
+}
---
+++ src/br/display/Display.java
@@ -0,0 +1,30 @@
+package br.display;
+
+import java.awt.Canvas;
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+public class Display {
+	
+	private JFrame jframe;
+	private Canvas canvas;
+	
+	public Display(String title, int width, int height) {
+		
+		canvas = new Canvas();
+		canvas.setPreferredSize(new Dimension(width, height));
+		canvas.setMaximumSize(new Dimension(width, height));
+		canvas.setMinimumSize(new Dimension(width, height));
+		
+		jframe = new JFrame(title);
+		jframe.add(canvas);
+		jframe.pack();
+		jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		jframe.setResizable(false);
+		jframe.setLocationRelativeTo(null);
+		jframe.setVisible(true);
+		
+
+	}
+}
